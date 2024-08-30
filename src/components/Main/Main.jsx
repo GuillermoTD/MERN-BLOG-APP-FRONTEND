6594a854b87119679a4859db3932df49b9f4337c
GuillermoTD <guillermotd23@gmail.com>
2024-08-30 17:42:38 -0400

    se le pone estilo al componente BlogItem

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Main = ({children}) => {
   return (
-    <div className='pt-[1rem]'>
+    <div className='pt-[1rem] bg-slate-100'>
         {children}
         
     </div>
